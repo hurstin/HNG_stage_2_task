a48e5196d576919f809847fe109e563c116ffea8
hurstin <132925282+hurstin@users.noreply.github.com>
2025-10-25 17:26:18 +0100

    added estimate_gdp field to country

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,15 +2,37 @@ import axios from 'axios';
 
 export const getCountries = async (req, res) => {
   try {
+    // Fetch country data from the REST Countries API
     const response = await axios.get(
       'https://restcountries.com/v2/all?fields=name,capital,region,population,flag,currencies'
     );
     const countries = response.data;
-    const currCode = countries.map((country) => {
-      const curCode = country.currencies?.[0]?.code;
-      return curCode;
+
+    // Fetch exchange rates from the Exchange Rate API
+    const exchangeRateResponse = await axios.get(
+      'https://open.er-api.com/v6/latest/USD'
+    );
+    const exchangeRates = exchangeRateResponse.data.rates;
+
+    // Process each country to calculate estimated GDP
+    const countriesWithGDP = countries.map((country) => {
+      // Get the currency code of the country
+      const currencyCode = country.currencies?.[0]?.code;
+      // Get the exchange rate for the currency, default to 1 if not found
+      const exchangeRate = exchangeRates[currencyCode] || 1;
+      // Generate a random number between 1000 and 2000
+      const randomNumber = Math.floor(Math.random() * (2000 - 1000 + 1)) + 1000;
+      // Calculate the estimated GDP
+      const estimated_gdp = (country.population * randomNumber) / exchangeRate;
+      // Return the country object with the estimated GDP
+      return {
+        ...country,
+        estimated_gdp,
+      };
     });
-    res.json(currCode);
+
+    // Send the processed country data with estimated GDP as a JSON response
+    res.json(countriesWithGDP);
   } catch (error) {
     console.error(error);
     res.status(500).send('An error occurred while fetching country data');
@@ -23,7 +45,9 @@ export const dummyController = (req, res) => {
 
 export const getExchangeRate = async (req, res) => {
   try {
-    const exchangeRateResponse = await axios.get('https://open.er-api.com/v6/latest/USD');
+    const exchangeRateResponse = await axios.get(
+      'https://open.er-api.com/v6/latest/USD'
+    );
     const exchangeRates = exchangeRateResponse.data.rates;
 
     const countriesResponse = await axios.get(
@@ -32,7 +56,7 @@ export const getExchangeRate = async (req, res) => {
     const countries = countriesResponse.data;
 
     const currencyRates = {};
-    countries.forEach(country => {
+    countries.forEach((country) => {
       if (country.currencies && country.currencies.length > 0) {
         const currencyCode = country.currencies[0].code;
         if (exchangeRates[currencyCode]) {
