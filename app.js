c928f91098fdb5e6f3f9cb1a7e7d51eeaea360f8
hurstin <132925282+hurstin@users.noreply.github.com>
2025-10-27 12:40:50 +0100

    change port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import dummyRoute from './routes/route.js';
 import sequelize from './db.js';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.use('/', dummyRoute);
 
